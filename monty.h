87f8e801006e8be3c347d295eac38a0bc3821774
SireCourage001 <sirecourage@gmail.com>
2023-10-22 21:21:17 -0700

    monty_task

--- monty.h
+++ monty.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
